ef52f9639a46ee6a47150120f5a5912b31b980b3
CYRUSM23 <cyrusmabulay@gmail.com>
2021-10-31 21:12:24 +0800

    updates

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+new Vue({
+    el: '#app', 
+    vuetify: new Vuetify(),
+    data: () => ({
+        overlay: true,
+      }),  
+      created() { 
+         setTimeout(() => {
+            this.overlay = false
+          }, 3000) 
+      },  
+  })  
